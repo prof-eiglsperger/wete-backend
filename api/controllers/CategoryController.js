362e2ec90f6d8dc3c55a06340d49f5ebb85352c5
Markus Eiglsperger <meiglspe@htwg-konstanz.de>
2024-09-25 17:08:41 +0200

    Initial commit

---
+++ api/controllers/CategoryController.js
@@ -0,0 +1,30 @@
+/**
+ * CategoryController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  create: async function (req, res) {
+    sails.log.debug("Create category....");
+    let params = req.allParams();
+    await Category.create(params);
+    res.ok();
+  },
+  find: async function (req, res) {
+    sails.log.debug("List all categories....");
+    let categories = await Category.find();
+    return res.json(categories);
+  },
+  findOne: async function (req, res) {
+    sails.log.debug("List single category....");
+    let category = await Category.findOne({ id: req.params.id });
+    res.json(category);
+  },
+  destroy: async function (req, res) {
+    sails.log.debug("Delete category....");
+    await Category.destroy({ id: req.params.id });
+    res.ok();
+  },
+};
